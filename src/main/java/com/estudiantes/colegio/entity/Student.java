692b14a9501b9a1de6347267ac5eb12e148f4f0c
Milton Figueredo Miles <gratno@gmail.com>
2023-06-15 18:22:11 -0500

    Create DB Student person

--- src/main/java/com/estudiantes/colegio/entity/Student.java
+++ src/main/java/com/estudiantes/colegio/entity/Student.java
@@ -1,4 +1,19 @@
 package com.estudiantes.colegio.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data //No es necsario los Set y Get
+@Entity
+@Table(name = "tbl_student")
 public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long studentID;
+    private String firstname;
+    private String lastname;
+    @Column(name = "email_addres", unique = true)
+    private String email;
+
 }
